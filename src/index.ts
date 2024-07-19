139fbe66f532017497841a3b8579e878fe982356
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-07-19 11:57:24 +0530

    Prisma Setup ✅

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-
+import { prismaClient } from "./lib/db";
 
 async function main() {
   const app = express();
@@ -9,25 +9,43 @@ async function main() {
 
   app.use(express.json());
 
-
-  //Graphql Server
-
+  // GraphQL Server
   const gqlserver = new ApolloServer({
     typeDefs: `
       type Query {
-        hello:String,
-        say(name:String):String
+        hello: String,
+        say(name: String): String
+      }
+      type Mutation {
+        createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
       }
     `,
     resolvers: {
-        Query :{
-            hello: ()=> `hey!🙋🏻‍♂️ I am Graphql Server`,
-            say: (_,{name}: {name:String})=>  `hey!🙋🏻‍♂️ ${name}. How's day Going?`,
-        }
+      Query: {
+        hello: () => `hey!🙋🏻‍♂️ I am Graphql Server`,
+        say: (_, { name }: { name: string }) => `hey!🙋🏻‍♂️ ${name}. How's day Going?`,
+      },
+      Mutation: {
+        createUser: async (
+          _: any,
+          { firstName, lastName, email, password }: { firstName: string; lastName: string; email: string; password: string }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
+      },
     },
   });
 
-  //Start  gql server
+  // Start gql server
   await gqlserver.start();
 
   app.use("/graphql", expressMiddleware(gqlserver));
@@ -40,6 +58,7 @@ async function main() {
     console.log(`Server is Running at PORT NO: ${PORT}`);
   });
 }
+
 main().catch((error) => {
   console.error("Error starting server:", error);
 });
